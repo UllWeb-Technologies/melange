7fbc4a21cb5cca74cf34c18268526dd6f0feffc6
Unyime <cornellkid2@gmail.com>
2022-01-17 16:34:54 +0100

    Update

--- src/Pages/Hero/Hero.js
+++ src/Pages/Hero/Hero.js
@@ -5,7 +5,7 @@ import '../Hero/css/hero.css'
 export default function Hero() {
     return (
         <div className="bg-div">
-            <main className="d-flex align-items-center p-5 col-12" style={{ height: '598px', backgroundImage: `url(${process.env.PUBLIC_URL + '/hero.png'})` }}>
+            <main className="d-flex align-items-center p-5 col-12" style={{ height: '100vh', backgroundImage: `url(${process.env.PUBLIC_URL + '/hero.png'})` }}>
                 <section className='d-flex text-center align-items-center justify-content-center flex-column col-12'>
                     <h2>welcome to Melange <br /> projects</h2>
                     <Button className="hero-btn mt-3">More</Button>
